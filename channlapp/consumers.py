07501c87451c6b0ef803373723df0a69c68098fe
Dean Symington <dean@leadgenius.com>
2016-09-12 15:48:51 +0200

    updated to use asgi_redis

--- channlapp/consumers.py
+++ channlapp/consumers.py
@@ -1,17 +1,26 @@
 from django.http import HttpResponse
 from channels.handler import AsgiHandler
 from channels import Group
+from channels.sessions import channel_session
 
+@channel_session
+def ws_connect(message):
+  room = message.content['path'].strip("/")
+  message.channel_session['room'] = room
+  Group("chat-%s" % room).add(message.reply_channel)
+
+@channel_session
 def ws_message(message):
-    Group("chat").send({
-        "text": "[user] %s" % message.content['text'],
-    })
+  Group("chat-%s" % message.channel_session['room']).send({
+        "text": message['text'],
+  })
 
 
 # Connected to websocket.connect
 def ws_add(message):
-    Group("chat").add(message.reply_channel)
+  Group("chat").add(message.reply_channel)
 
 # Connected to websocket.disconnect
+@channel_session
 def ws_disconnect(message):
-    Group("chat").discard(message.reply_channel)
+  Group("chat-%s").discard(message.reply_channel)
